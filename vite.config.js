8e119002be2dbca5122ffa68b1605c1b3414039f
Rytham <frellancerytham@gmail.com>
2026-07-09 20:26:20 +0530

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const backendTarget = process.env.VITE_DEV_BACKEND_URL || 'http://127.0.0.1:10000';
+const backendTarget = process.env.VITE_DEV_BACKEND_URL || 'http://localhost:5100';
 
 // https://vite.dev/config/
 export default defineConfig({
